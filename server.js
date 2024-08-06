d3a799ac24fa2373755285f2efbc325d9a35f582
Gravqc <gauravgowda452@gmail.com>
2024-08-06 08:53:27 +0530

    add few middlewares

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-const express = require("express")
+const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 
 const dotenv = require("dotenv").config()
 const app = express()
 
 const port = process.env.PORT || 5000;
-//middleware
-app.use("/api/contacts", require("./routes/contactRoutes"))
 
+app.use(express.json())//middleware used to parse incoming requests with json payloads
+// basically will convert the request into a JSON object
+
+app.use("/api/contacts", require("./routes/contactRoutes"))
+app.use(errorHandler)
 app.listen(port, () =>{
     console.log(`server is listening ${port}`)
 })
