861c15675a26b1e8ec1fadd2d4fde818df59fcd1
Gravqc <gauravgowda452@gmail.com>
2024-08-07 21:47:04 +0530

    connecting to db

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
+const connectDb = require("./config/dbConnection");
 
 const dotenv = require("dotenv").config()
 const app = express()
 
 const port = process.env.PORT || 5000;
+
+connectDb();
 
 app.use(express.json())//middleware used to parse incoming requests with json payloads
 // basically will convert the request into a JSON object
